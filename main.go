cfc283643f188c7bd5c51bcbc930b24b5425f52e
Diego Leon <diegoleon.77@hotmail.com>
2022-11-04 20:32:52 -0500

    JSON encoding

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type Customer struct {
+	Name    string `json:"name,omitempty"`
+	City    string `json:"city,omitempty"`
+	Zipcode string `json:"zipcode,omitempty"`
+}
+
 func main() {
 	//define routes
 	http.HandleFunc("/greet", greet)
+	http.HandleFunc("/customers", getAllCustomers)
 
 	//starting server
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
@@ -16,4 +24,13 @@ func main() {
 
 func greet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello world!!!")
+}
+
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{"Diego", "Giron", "68003"},
+		{"Angie", "Madrid", "88001"},
+	}
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(customers)
 }
