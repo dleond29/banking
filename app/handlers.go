9609d9fcd9ec9b2d6746926bb67d65fd32621c03
Diego Leon <diegoleon.77@hotmail.com>
2022-11-04 21:14:54 -0500

    Refactoring code

---
+++ app/handlers.go
@@ -0,0 +1,34 @@
+package app
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"net/http"
+)
+
+type Customer struct {
+	Name    string `json:"name,omitempty" xml:"name"`
+	City    string `json:"city,omitempty" xml:"city"`
+	Zipcode string `json:"zipcode,omitempty" xml:"zipcode"`
+}
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello world!!!")
+}
+
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{"Diego", "Giron", "68003"},
+		{"Angie", "Madrid", "88001"},
+	}
+
+	if r.Header.Get("Content-Type") == "application/xml" {
+		w.Header().Add("Content-Type", "application/xml")
+		xml.NewEncoder(w).Encode(customers)
+	} else {
+		w.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(customers)
+	}
+
+}
--- main.go
+++ main.go
@@ -1,44 +1,5 @@
 package main
 
-import (
-	"encoding/json"
-	"encoding/xml"
-	"fmt"
-	"log"
-	"net/http"
-)
-
-type Customer struct {
-	Name    string `json:"name,omitempty" xml:"name"`
-	City    string `json:"city,omitempty" xml:"city"`
-	Zipcode string `json:"zipcode,omitempty" xml:"zipcode"`
-}
-
 func main() {
-	//define routes
-	http.HandleFunc("/greet", greet)
-	http.HandleFunc("/customers", getAllCustomers)
-
-	//starting server
-	log.Fatal(http.ListenAndServe("localhost:8000", nil))
-}
-
-func greet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello world!!!")
-}
-
-func getAllCustomers(w http.ResponseWriter, r *http.Request) {
-	customers := []Customer{
-		{"Diego", "Giron", "68003"},
-		{"Angie", "Madrid", "88001"},
-	}
-
-	if r.Header.Get("Content-Type") == "application/xml" {
-		w.Header().Add("Content-Type", "application/xml")
-		xml.NewEncoder(w).Encode(customers)
-	} else {
-		w.Header().Add("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(customers)
-	}
-
+	app.Start()
 }
